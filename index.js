2f6eae32b73f84e5794d6a16a76bd33c8a339875
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-19 16:46:52 +0500

    searching and sorting games based on their platforms function done

--- index.js
+++ index.js
@@ -125,3 +125,32 @@ const fetchAndDisplayGames = async (searchQuery = '') => {
         console.error('Error fetching and displaying games:', error);
     }
 };
+
+//! Function to show games based on their platforms
+
+const filterByPlatform = async (platform) => {
+    const gameList = document.getElementById("gameList");
+    gameList.innerHTML = ""; 
+    
+    try {
+        const response = await fetch(`https://api.rawg.io/api/games?key=460cd17137bd4114915dfd76478c1ba7&platforms=${platform}`);
+        const data = await response.json();
+        data.results.forEach(game => {
+            const gameCard = document.createElement("div");
+            gameCard.classList.add("col-md-4", "mb-4");
+            gameCard.innerHTML = `
+            <div class="card">
+            <div class="card-header" style="background-image: url('${game.background_image}'); height: 200px; background-size: cover; background-position: center;"></div>
+            <div class="card-body">
+            <h5 class="card-title text-center"> <a href="game-details.html?id=${game.id}">${game.name}</a> </h5>
+            <p class="card-text d-inline-block ms-5 me-5">ID: ${game.id}</p>
+            <p class="card-text d-inline-block ms-5 text-end">Rating: ${game.rating}</p>
+            </div>
+            </div>
+            `;
+            gameList.appendChild(gameCard);
+        });
+    } catch (error) {
+        console.error('Error fetching and displaying games:', error);
+    }
+};
