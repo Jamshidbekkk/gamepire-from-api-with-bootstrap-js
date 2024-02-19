8937312802feda9fba191555c6bbfc176d47c5db
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-19 16:55:18 +0500

    project finished

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ darkModeToggle.addEventListener("change", () => {
 
 //! Searching for a game via search input function
 const searchGames = async () => {
-
+    
     const searchInput = document.getElementById("searchInput").value.trim().toLowerCase();
     const gameList = document.getElementById("gameList");
     gameList.innerHTML = ""; 
-
+    
     try {
         const response = await fetch(`https://api.rawg.io/api/games?key=460cd17137bd4114915dfd76478c1ba7&search=${searchInput}`);
         const data = await response.json();
@@ -68,12 +68,12 @@ const searchGames = async () => {
             gameCard.classList.add("col-md-4", "mb-4");
             gameCard.innerHTML = `
             <div class="card">
-                <div class="card-header" style="background-image: url('${game.background_image}'); height: 200px; background-size: cover; background-position: center;"></div>
-                <div class="card-body">
-                    <h5 class="card-title text-center"> <a href="game-details.html?id=${game.id}">${game.name} </a> </h5>
-                    <p class="card-text d-inline-block ms-5 me-5">ID: ${game.id}</p>
-                    <p class="card-text d-inline-block ms-5 text-end">Rating: ${game.rating}</p>
-                </div>
+            <div class="card-header" style="background-image: url('${game.background_image}'); height: 200px; background-size: cover; background-position: center;"></div>
+            <div class="card-body">
+            <h5 class="card-title text-center"> <a href="game-details.html?id=${game.id}">${game.name} </a> </h5>
+            <p class="card-text d-inline-block ms-5 me-5">ID: ${game.id}</p>
+            <p class="card-text d-inline-block ms-5 text-end">Rating: ${game.rating}</p>
+            </div>
             </div>
             `;
             gameList.appendChild(gameCard);
