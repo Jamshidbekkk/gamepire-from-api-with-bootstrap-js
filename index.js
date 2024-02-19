ef9ba4d6eb1bae9d0ebc3cea79a9b02d47952475
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-19 16:45:58 +0500

    searching for games based on their genres function done

--- index.js
+++ index.js
@@ -83,3 +83,45 @@ const searchGames = async () => {
     }
 };
 
+//! Functions to find the games of one genre type once the genre has been clicked
+let currentGenre = null; 
+
+const filterByGenre = async (genre) => {
+    currentGenre = genre;
+    await fetchAndDisplayGames();
+};
+
+const fetchAndDisplayGames = async (searchQuery = '') => {
+    const gameList = document.getElementById("gameList");
+    gameList.innerHTML = ""; 
+    
+    try {
+        let apiUrl = "https://api.rawg.io/api/games?key=460cd17137bd4114915dfd76478c1ba7";
+        if (currentGenre) {
+            apiUrl += `&genres=${currentGenre}`;
+        }
+        if (searchQuery) {
+            apiUrl += `&search=${searchQuery}`;
+        }
+        
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        data.results.forEach(game => {
+            const gameCard = document.createElement("div");
+            gameCard.classList.add("col-md-4", "mb-4");
+            gameCard.innerHTML = `
+            <div class="card">
+            <div class="card-header" style="background-image: url('${game.background_image}'); height: 200px; background-size: cover; background-position: center;"></div>
+            <div class="card-body">
+            <h5 class="card-title text-center cursor-pointer"><a href="game-details.html?id=${game.id}">${game.name}</a></h5>
+            <p class="card-text d-inline-block ms-5 me-5">ID: ${game.id}</p>
+            <p class="card-text d-inline-block ms-5 text-end">Rating: ${game.rating}</p>
+            </div>
+            </div>
+            `;
+            gameList.appendChild(gameCard);
+        });
+    } catch (error) {
+        console.error('Error fetching and displaying games:', error);
+    }
+};
